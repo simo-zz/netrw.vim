c7d6383494e1ed94caa9e96acb837d2e55f4148e
Luca Saccarola <github.e41mv@aleeas.com>
2025-01-17 22:06:11 +0100

    feat: make compare script more structured

--- scripts/compare_with_vim.sh
+++ scripts/compare_with_vim.sh
@@ -1,36 +1,59 @@
 #!/usr/bin/env bash
 
-readonly SCRIPT_PWD="${SCRIPT_PWD:-$(cd "$(dirname "$0")/.." && pwd)}"
+set -eu
+
+msg_ok() { printf '\e[32m✔\e[0m %s\n' "$1"; }
+msg_err() { printf '\e[31m✘\e[0m %s\n' "$1" >&2; }
+panic() { msg_err "Panic: $1" && exit 1; }
+check_executable() { type "$1" || panic "$1 not found in PATH or not executable."; }
+
+# Ensure that the user has a bash that supports -A
+[[ ${BASH_VERSINFO[0]} -ge 4 ]] ||
+    panic "script requires bash 4+ (you have ${BASH_VERSION})."
+
+readonly SCRIPT_PWD=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." &>/dev/null && pwd)
 readonly VIM_SRC_DIR="$SCRIPT_PWD/.repos/vim"
-readonly NETRW_VERSION=$(git tag | tail -n1 | tr -d v)
 
-if [ -d "$VIM_SRC_DIR" ]; then
-    git -C "$VIM_SRC_DIR" pull --depth=1 --rebase origin master
-else
-    git clone "https://github.com/vim/vim.git" "$VIM_SRC_DIR"
-fi
+update_vim_source() {
+    check_executable git
 
-files=(
-    'autoload/netrw.vim'
-    'autoload/netrwSettings.vim'
-    'autoload/netrw_gitignore.vim'
-    'syntax/netrw.vim'
-    'plugin/netrwPlugin.vim'
-    'doc/netrw.txt'
-)
-
-for file in ${files[@]}; do
-    if grep -q "v$NETRW_VERSION" "$file"; then
-        echo "You must update the version before continuing"
-        exit 1
+    if [[ ! -d $VIM_SRC_DIR ]]; then
+        echo "Cloning Vim into: $VIM_SRC_DIR"
+        git clone https://github.com/vim/vim.git "$VIM_SRC_DIR"
+    else
+        if [[ ! -d ".git" && "$(git rev-parse --show-toplevel)" != "$VIM_SRC_DIR" ]]; then
+            panic "$VIM_SRC_DIR does not appear to be a git repository."
+        fi
+        echo "Updating Vim sources: $VIM_SRC_DIR"
+        if git pull --ff; then
+            msg_ok "Updated Vim sources."
+        else
+            msg_err "Could not update Vim sources; ignoring error."
+        fi
     fi
-done
+}
 
-for file in ${files[@]}; do
-    cp -f "$SCRIPT_PWD/$file" "$VIM_SRC_DIR/runtime/pack/dist/opt/netrw/$file"
-done
+update_source_files() {
+    local files
+    files=(
+        'autoload/netrw.vim'
+        'autoload/netrwSettings.vim'
+        'autoload/netrw_gitignore.vim'
+        'syntax/netrw.vim'
+        'plugin/netrwPlugin.vim'
+        'doc/netrw.txt'
+    )
 
-cd "$VIM_SRC_DIR" || exit 1
+    for file in "${files[@]}"; do
+        cp -f "$SCRIPT_PWD/$file" "$VIM_SRC_DIR/runtime/pack/dist/opt/netrw/$file"
+    done
 
-git add --all
-git commit -e -m "runtime(netrw): upstream snapshot of v$(($NETRW_VERSION + 1))"
+    msg_ok "Updated Source files."
+}
+
+main() {
+    update_vim_source
+    update_source_files
+}
+
+main
